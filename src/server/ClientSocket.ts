adab7f0e27c0e0f1f7c4d2689da272c3f7e5f69e
teds <264091602+RareCityRS@users.noreply.github.com>
2026-09-08 22:14:15 +0930

    Bank space fix (engine)

--- src/server/ClientSocket.ts
+++ src/server/ClientSocket.ts
@@ -4,7 +4,6 @@ import { NetworkPlayer } from '#/engine/entity/NetworkPlayer.js';
 import Isaac from '#/io/Isaac.js';
 import Packet from '#/io/Packet.js';
 
-
 export default abstract class ClientSocket {
     uuid = randomUUID();
     remoteAddress = 'unknown';
@@ -17,7 +16,7 @@ export default abstract class ClientSocket {
     decryptor: Isaac | null = null;
 
     in = Packet.alloc(65535); // node won't let us read from the socket as a stream so we buffer it ourselves
-    out = Packet.alloc(1);
+    out = Packet.alloc(2);
 
     opcode = -1; // current opcode being read
     waiting = 0; // bytes to wait for (if any)
